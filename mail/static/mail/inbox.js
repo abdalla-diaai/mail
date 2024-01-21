28150afc5c92c8a6f161df520af6ca85ca572f8d
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-01-21 04:19:45 +0000

    finalised project and added some styles.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,9 +1,11 @@
+// variable to show or remove reply button
 var reply = false;
+
+// listner to load page
 document.addEventListener('DOMContentLoaded', function () {
     window.onpopstate = function (event) {
         loadMailbox(event.state.section);
     };
-    
     document.querySelectorAll('button').forEach(button => {
         button.onclick = function () {
             const section = this.dataset.section;
@@ -20,6 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
     loadMailbox('inbox');
 });
 
+// function to handle email compose and reply
 function composeEmail(email, reply) {
     // Show compose view and hide other views
     document.querySelector('#emails-view').style.display = 'none';
@@ -32,12 +35,17 @@ function composeEmail(email, reply) {
     } else {
         document.querySelector('#compose-body').style.color = 'black';
         document.querySelector('#compose-recipients').value = email.sender;
-        document.querySelector('#compose-subject').value = `RE: ${email.subject}`;
+        if (!email.subject.startsWith('RE:')) {
+            document.querySelector('#compose-subject').value = `RE: ${email.subject}`;
+        } else {
+            document.querySelector('#compose-subject').value = `${email.subject}`;
+        };
         document.querySelector('#compose-body').value = `\n on ${email.timestamp} ${email.sender} wrote: ${email.body}`;
     }
-    document.querySelector('#compose-form').addEventListener('submit', send_email);
+    document.querySelector('#compose-form').addEventListener('submit', sendEmail);
 };
 
+// function to load mailbox
 function loadMailbox(mailbox) {
     // get current user
     const currentUser = document.querySelector('#user-email').textContent;
@@ -49,7 +57,31 @@ function loadMailbox(mailbox) {
 
     // Show the mailbox name
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-    const mailboxName = document.querySelector('h3');
+    const tableHeadingNames = ['', 'Subject', 'Time', '', ''];
+    var mailboxName = document.querySelector('h3');
+    const emailsList = document.createElement('table');
+    emailsList.classList.add('table', 'table-striped', 'table-hover');
+    const tableHeading = document.createElement('thead');
+    const headingRow = document.createElement('tr');
+    tableHeading.append(headingRow);
+    for (var i = 0; i < 5; i++) {
+        const temp = document.createElement('th');
+        temp.setAttribute('scope', 'col');
+        if (i === 0) {
+            if (mailboxName.innerText === 'Sent') {
+                temp.textContent = 'To';
+            } else {
+                temp.textContent = 'From';
+            };
+        } else {
+            temp.textContent = tableHeadingNames[i];
+        };
+        headingRow.append(temp);
+    };
+    const tableBody = document.createElement('tbody');
+    emailsList.append(tableHeading);
+    emailsList.append(tableBody);
+
     fetch(`/emails/${mailbox}`)
         .then(response => response.json())
         .then(emails => {
@@ -58,47 +90,67 @@ function loadMailbox(mailbox) {
                 if (email.read === false) {
                     emailCounter++;
                 };
+                // add email number to button
                 document.querySelector('.badge').textContent = emailCounter;
-                const emailRow = document.createElement('div');
-                emailRow.classList.add('col-9', 'single-email');
-                emailRow.setAttribute("data-id", email.id);
+                const emailRow = document.createElement('tr');
+                emailRow.classList.add('single-email');
+                emailRow.setAttribute('data-id', email.id);
+                tableBody.append(emailRow);
+                const emailSender = document.createElement('td');
+                emailSender.setAttribute('class', 'email-sender');
+                const emailSubject = document.createElement('td');
+                const emailTimeStamp = document.createElement('td');
+                const emailRead = document.createElement('td');
+                emailRow.append(emailSender);
+                emailRow.append(emailSubject);
+                emailRow.append(emailTimeStamp);
+                emailRow.append(emailRead);
                 const readSpan = document.createElement('span');
-                readSpan.classList.add('col-3', 'badge', 'text-bg-info');
-                
+                readSpan.setAttribute('id', 'email-badge');
+                emailRead.append(readSpan);
+                if (mailboxName.innerText === 'Sent') {
+                    emailSender.textContent = email.recipients;
+                }
+                else {
+                    emailSender.textContent = email.sender;
+                };
+                emailSubject.textContent = email.subject;
+                emailTimeStamp.textContent = email.timestamp;
                 // check mailbox
-                if (mailboxName.innerText.toLocaleLowerCase() === 'inbox' || mailboxName.innerText.toLocaleLowerCase() === 'archive') {
+                if (mailboxName.innerText === 'Inbox' || mailboxName.innerText === 'Archive') {
                     // check user
                     if (email.read === false) {
+                        readSpan.classList.add('badge', 'text-bg-info');
                         readSpan.textContent = "Unread";
                     } else {
                         readSpan.textContent = "Read";
-                    };               
+                        readSpan.classList.add('badge', 'text-bg-light');
+                    };
                 };
+                document.querySelector('#emails-view').append(emailsList);
+
                 emailRow.addEventListener('click', function (event) {
                     if (email.recipients == currentUser) {
                         setTimeout(() => {
-                            emailRead(emailId, true);
+                            MarkEmailRead(email.id, true);
                         }, 500)
                     };
-                    const emailId = event.target.dataset.id;
-                    const baseUrl = "/emails";
-                    history.replaceState({ 'emailId': emailId }, "", baseUrl + '/' + email.id);
+                    history.replaceState({ 'email': event.target.dataset.id }, "", '/emails' + '/' + email.id);
                     document.querySelector('#emails-view').style.display = 'none';
                     document.querySelector('#show-email').style.display = 'block';
                     document.querySelector('#compose-view').style.display = 'none';
                     if (email.read === false) {
                         emailCounter--;
                     };
-                    viewEmail(emailId);
+                   
+                    viewEmail(email.id);
                 });
-                emailRow.innerHTML = `<strong>${email.recipients}</strong> ${email.subject} ${email.timestamp} ${readSpan.textContent}`;
-                document.querySelector('#emails-view').append(emailRow);
-
             });
         });
 };
 
-function send_email(event) {
+// function to send emails
+function sendEmail(event) {
     // stop form from submission then upate
     event.preventDefault();
     fetch('/emails', {
@@ -121,6 +173,7 @@ function send_email(event) {
         });
 };
 
+// function to view single email
 function viewEmail(id) {
     // get elements from page
     var showEmailDiv = document.querySelector('#show-email')
@@ -134,7 +187,7 @@ function viewEmail(id) {
     if (!emailDetails && !emailBody) {
         emailDetails = document.createElement('div');
         emailDetails.setAttribute('id', 'details');
-        emailBody = document.createElement('div');
+        emailBody = document.createElement('p');
         emailBody.setAttribute('id', 'body');
         replyBtn = document.createElement('button');
         replyBtn.setAttribute('id', 'reply-button');
@@ -158,8 +211,8 @@ function viewEmail(id) {
         .then(response => response.json())
         .then(email => {
             email = JSON.parse(JSON.stringify(email));
-            emailDetails.innerText = `
-            From: ${email.sender}\nTo: ${email.recipients}\nSubject: ${email.subject}\nTimestamp: ${email.timestamp}`;
+            const emailDetailsText = `From: ${email.sender}\nTo: ${email.recipients}\nSubject: ${email.subject}\nTimestamp: ${email.timestamp}`;
+            emailDetails.innerText = emailDetailsText;
             emailBody.innerText = `${email.body} \n`;
             if (pageHeading.innerText.toLocaleLowerCase() === 'sent') {
                 replyBtn.style.display = 'none';
@@ -168,7 +221,6 @@ function viewEmail(id) {
             } else {
                 // change button display based on page
                 if (pageHeading.innerText.toLocaleLowerCase() === 'archive') {
-                    console.log('true');
                     archiveBtn.style.display = 'none';
                     unarchiveBtn.style.display = 'inline-block';
                 } else {
@@ -204,6 +256,7 @@ function viewEmail(id) {
         });
 };
 
+// function to archive emails
 function archiveEmail(id, state) {
     fetch(`/emails/${id}`, {
         method: 'PUT',
@@ -213,7 +266,8 @@ function archiveEmail(id, state) {
     });
 };
 
-function emailRead(id, state) {
+// function to mark emails as read
+function MarkEmailRead(id, state) {
     fetch(`/emails/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
@@ -222,3 +276,7 @@ function emailRead(id, state) {
     });
 };
 
+function boldString(str, find){
+    var re = new RegExp(find, 'g');
+    return str.replace(re, '<b>'+find+'</b>');
+}
